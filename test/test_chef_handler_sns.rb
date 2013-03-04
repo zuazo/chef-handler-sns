1851d01a5150265e994ebf78bbf1f324c3bdee5e
Xabier de Zuazo <xabier@onddo.com>
2013-03-04 19:39:27 +0100

    more tests added for the Chef::Handler::Sns class

--- test/test_chef_handler_sns.rb
+++ test/test_chef_handler_sns.rb
@@ -2,6 +2,14 @@ require 'helper'
 require 'chef/node'
 require 'chef/run_status'
 
+class RightAws::FakeSnsInterface < RightAws::SnsInterface
+  attr_reader :sns_interface_new, :topic_arn, :message, :subject
+  def fake_new
+    @sns_interface_new = true
+    return self
+  end
+end
+
 describe Chef::Handler::Sns do
   before do
     RightAws::SnsInterface.any_instance.stubs(:publish).returns(true)
@@ -13,17 +21,59 @@ describe Chef::Handler::Sns do
     @run_status = Chef::RunStatus.new(@node, {})
     @run_status.start_clock
     @run_status.stop_clock
+
+    @config = {
+      :access_key => '***AMAZON-KEY***',
+      :secret_key => '***AMAZON-SECRET***',
+      :topic_arn => 'arn:aws:sns:***',
+    }
   end
 
-  it 'sends a SNS message when properly configured' do
-    @sns_handler = Chef::Handler::Sns.new
+  it 'should read the configuration options on initialization' do
+    @sns_handler = Chef::Handler::Sns.new(@config)
+    assert_equal @sns_handler.access_key, @config[:access_key]
+    assert_equal @sns_handler.secret_key, @config[:secret_key]
+  end
 
-    @sns_handler.access_key('***AMAZON-KEY***')
-    @sns_handler.secret_key('***AMAZON-SECRET***')
-    @sns_handler.topic_arn('arn:aws:sns:***')
+  it 'should be able to change configuration options using method calls' do
+    @sns_handler = Chef::Handler::Sns.new
+    @sns_handler.access_key(@config[:access_key])
+    @sns_handler.secret_key(@config[:secret_key])
+    assert_equal @sns_handler.access_key, @config[:access_key]
+    assert_equal @sns_handler.secret_key, @config[:secret_key]
+  end
+
+  it 'should try to send a SNS message when properly configured' do
+    @sns_handler = Chef::Handler::Sns.new(@config)
     RightAws::SnsInterface.any_instance.expects(:publish).once
 
     @sns_handler.run_report_safely(@run_status)
+  end
+
+  it 'should create a RightAws::SnsInterface object' do
+    @sns_handler = Chef::Handler::Sns.new(@config)
+    fake_sns = RightAws::FakeSnsInterface.new(@config[:access_key], @config[:secret_key], {:logger => Chef::Log})
+    RightAws::SnsInterface.any_instance.stubs(:new).returns(fake_sns.fake_new)
+    @sns_handler.run_report_safely(@run_status)
+
+    assert_equal fake_sns.sns_interface_new, true
+  end
+
+  it 'should detect the AWS region automatically' do
+    @node.set['ec2']['placement_availability_zone'] = 'eu-west-1a'
+    @sns_handler = Chef::Handler::Sns.new(@config)
+    @sns_handler.run_report_safely(@run_status)
+
+    @sns_handler.server.must_match Regexp.new('eu-west-1')
+  end
+
+  it 'should not detect AWS region automatically whan manually set' do
+    @node.set['ec2']['placement_availability_zone'] = 'eu-west-1a'
+    @config[:region] = 'us-east-1'
+    @sns_handler = Chef::Handler::Sns.new(@config)
+    @sns_handler.run_report_safely(@run_status)
+
+    @sns_handler.server.must_match Regexp.new('us-east-1')
   end
 
 end
