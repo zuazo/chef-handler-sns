7a8e16ed7096d2a5e617b07d3eca5935110be48a
Xabier de Zuazo <xabier@onddo.com>
2013-03-17 14:00:42 +0100

    set up coveralls

--- test/helper.rb
+++ test/helper.rb
@@ -1,6 +1,9 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
+require 'coveralls'
+Coveralls.wear!
+
 require 'rubygems'
 gem 'minitest' # ensures you're using the gem, and not the built in MT
 require 'minitest/autorun'
