22902d894b235ca67c2c342ea8858031a1250076
Xabier de Zuazo <xabier@onddo.com>
2013-10-27 07:27:24 +0100

    Removed require rubygems from test/helper

--- test/helper.rb
+++ test/helper.rb
@@ -4,7 +4,6 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'coveralls'
 Coveralls.wear!
 
-require 'rubygems'
 gem 'minitest' # ensures you're using the gem, and not the built in MT
 require 'minitest/autorun'
 require 'mocha/setup'
